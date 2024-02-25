e2cf77fed33755074936ac6dea59090f2c3c54d3
alex-kadur <122496220+alex-kadur@users.noreply.github.com>
2024-02-25 17:43:48 +0100

    Update einstein.py

--- Problem Sets/Week 0/Einstein/einstein.py
+++ Problem Sets/Week 0/Einstein/einstein.py
@@ -1,12 +1,14 @@
-#prompt user for `m` kilograms and output equivalent `E` Joules
+# prompt user for m kilograms and output equivalent E Joules
 def main():
     joules = int(input("m: "))
     print("E:", emc2(joules))
 
-#calculate equivalent number Joules via E=mc2
+
+# calculate equivalent number Joules via E=mc2
 def emc2(m):
-    c2 = pow(300000000,2)
+    c2 = pow(300000000, 2)
     return m * c2
 
-#call main
+
+# call main
 main()
