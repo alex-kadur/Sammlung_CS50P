2815b50cd4a25c3e06cdba79be4f63b731b5d8ff
Alexander Kadur <122496220+alex-kadur@users.noreply.github.com>
2024-02-28 15:52:01 +0100

    Update seasons.py

--- Problem Sets/Week 8/Seasons of Love/seasons.py
+++ Problem Sets/Week 8/Seasons of Love/seasons.py
@@ -7,6 +7,8 @@ import inflect
 p = inflect.engine()
 
 
+# prompts the user for their date of birth in YYYY-MM-DD format
+# print how old they are in minutes, rounded to the nearest integer, using English words instead of numerals
 def main():
     date_input = input("Date of Birth: ")
     date_valid = check_date(date_input)
@@ -15,6 +17,11 @@ def main():
     print(f"{age_convert.capitalize()} minutes")
 
 
+# convert user input into suitable arguments for datetime.date()
+# create date object from converted user input
+# assumes, for simplicity, that the user was born at midnight (i.e., 00:00:00)
+# catch invalid date by ValueError
+# return date object
 def check_date(date_input):
     date_match = re.search(r"^(\d{4})-(\d{2})-(\d{2})$", date_input)
     if date_match:
@@ -30,6 +37,11 @@ def check_date(date_input):
         sys.exit("Invalid date")
 
 
+# create date object for current date using classmethod date.today()
+# assumes, for simplicity, that the current time is midnight
+# calculate user age by subtracting input date object from current date object
+# overloaded `-` returns timedelta object
+# returns timedelta (age) in days as int
 def get_age(date_valid):
     date_current = date.today()
     age = date_current - date_valid
@@ -39,6 +51,9 @@ def get_age(date_valid):
         return int(age.days)
 
 
+# converts age in days to age in minutes
+# converts age from numerals to English words unsing inflect
+# returns age as English words
 def convert_age(age):
     age_minutes = age * 24 * 60
     age_words = p.number_to_words(age_minutes, andword="")
@@ -47,3 +62,4 @@ def convert_age(age):
 
 if __name__ == "__main__":
     main()
+
